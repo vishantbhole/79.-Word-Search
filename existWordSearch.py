0d9512fd7cc06109bada9d76f2836e140af92176
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-12-05 21:01:47 -0600

    Implement word search method in Solution class
    Added a new class Solution with a method to search for a word in a board.

--- existWordSearch.py
+++ existWordSearch.py
@@ -1,3 +1,7 @@
 
 #79. Word Search
 from typing import List
+
+class Solution:
+    def existWordSearch(self, board: List[List[str]], word: str) -> bool:
+        # O(n * m * 4^k) where k is len of word
