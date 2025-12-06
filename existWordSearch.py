8921a8ed8b4d0b7706fee10e50ccd613771bdcdb
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-12-05 21:02:42 -0600

    Add dimensions and dfs function to existWordSearch
    Initialize ROWS and COLS variables and define dfs function.

--- existWordSearch.py
+++ existWordSearch.py
@@ -5,3 +5,10 @@ from typing import List
 class Solution:
     def existWordSearch(self, board: List[List[str]], word: str) -> bool:
         # O(n * m * 4^k) where k is len of word
+        ROWS = len(board)
+        COLS = len(board[0])
+        path = set()
+
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
